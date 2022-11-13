5bea2922cfc4ace20f2d88b8f34d2bb199bf5bc8
Leonardo Alves <leonardoallves1992@gmail.com>
2022-11-13 07:47:13 -0300

    edição script conexão

--- src/php/conexao.php
+++ src/php/conexao.php
@@ -3,7 +3,7 @@
     $db_name = 'agenda-sistemas-pdo';
     $db_host = 'localhost';
     $db_user = 'root';
-    $db_pass = 'admin';
+    $db_pass = '';
 
     $conexao = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
 ?>
